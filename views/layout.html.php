c2dbc0a0d2878a8b79a67d8c56fe68a6c71bb863
jessicathi <jessica.thielemans@hetic.net>
2018-06-13 12:05:05 +0200

    add doctype + add link style

--- views/layout.html.php
+++ views/layout.html.php
@@ -1,43 +1,49 @@
-<style>
-<?php include 'assets/css/style.css'; ?>
-</style>
-
-<h1 class="title-page">Pimp my blog</h1>
-<nav class="nav-page">
-  <ul class="content-link-page">
-    <li class="link-page active">Home</li>
-    <li class="link-page">Page 1</li>
-    <li class="link-page">Page 2</li>
-    <li class="link-page">Page 3</li>
-    <li class="link-page">Page 4</li>
-    <li class="link-page">Page 5</li>
-  </ul>
-</nav>
-<div class="content-articles">
-  <div class="block-left">
-    <div class="">
-      <h2 class="title-article">Title article random news for random people</h2>
-      <p class="date-article">01/02/18</p>
-      <hr class="line-article">
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+    <h1 class="title-page">Pimp my blog</h1>
+    <nav class="nav-page">
+      <ul class="content-link-page">
+        <li class="link-page active">Home</li>
+        <li class="link-page">Page 1</li>
+        <li class="link-page">Page 2</li>
+        <li class="link-page">Page 3</li>
+        <li class="link-page">Page 4</li>
+        <li class="link-page">Page 5</li>
+      </ul>
+    </nav>
+    <div class="content-articles">
+      <div class="block-left">
+        <div class="">
+          <h2 class="title-article">Title article random news for random people</h2>
+          <p class="date-article">01/02/18</p>
+          <hr class="line-article">
+        </div>
+        <div class="">
+          <p class="text-article">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+        </div>
+        <div class="">
+          <p class="text-article">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate</p>
+        </div>
+      </div>
+      <div class="block-right">
+        <h2 class="title-proposition">Recommandation</h2>
+        <div class="article-proposition">
+          <h3 class="title-article-proposition">The great title</h3>
+          <p class="text-article-proposition">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <a href="#" class="link-article-proposition">Read it</a>
+        </div>
+        <div class="article-proposition">
+          <h3 class="title-article-proposition">The great title</h3>
+          <p class="text-article-proposition">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <a href="#" class="link-article-proposition">Read it</a>
+        </div>
+      </div>
     </div>
-    <div class="">
-      <p class="text-article">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-    </div>
-    <div class="">
-      <p class="text-article">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate</p>
-    </div>
-  </div>
-  <div class="block-right">
-    <h2 class="title-proposition">Recommandation</h2>
-    <div class="article-proposition">
-      <h3 class="title-article-proposition">The great title</h3>
-      <p class="text-article-proposition">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      <a href="#" class="link-article-proposition">Read it</a>
-    </div>
-    <div class="article-proposition">
-      <h3 class="title-article-proposition">The great title</h3>
-      <p class="text-article-proposition">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      <a href="#" class="link-article-proposition">Read it</a>
-    </div>
-  </div>
-</div>
+  </body>
+</html>
