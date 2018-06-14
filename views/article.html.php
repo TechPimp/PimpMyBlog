a266a693ba7404884ab40a97371681580765570d
Jeremy Razafinirina <jeremy@MacBook-Pro-de-Jeremy.local>
2018-06-14 15:45:59 +0200

    can get article by id

--- views/article.html.php
+++ views/article.html.php
@@ -1,3 +1,4 @@
+
 <h1 class="title-page" contenteditable="true">Pimp my blog</h1>
 <nav class="nav-page">
   <ul class="content-link-page">
@@ -11,18 +12,15 @@
 </nav>
 <div class="container">
     <div class="article">
-        <h2 class="article__title" contenteditable="true"> Title article randow news for random people </h2>
-        <span class="article__date" contenteditable="true"> 01/02/03</span>
+        <h2 class="article__title" contenteditable="true">
+            <?= $data['title']; ?>
+        </h2>
+        <span class="article__date" contenteditable="true">
+            <?= $data['date']; ?>
+        </span>
         <hr class="article__divider">
         <p class="article__item" contenteditable="true">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales neque eget consectetur condimentum. Phasellus sodales dignissim accumsan. Donec porta quis ipsum non feugiat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla at ligula porttitor, venenatis elit ut, lacinia mi. Praesent vel justo a urna convallis aliquam eu id nibh. Praesent sit amet porta diam. Aenean eget aliquet leo, vel molestie sapien. Donec tempus maximus orci. Nam malesuada, ex id eleifend fermentum, nisl odio fermentum quam, et sollicitudin enim ligula non ante. Duis feugiat et est eu malesuada. Nullam quis
-        </p>
-        <p class="article__item" contenteditable="true">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales neque eget consectetur condimentum. Phasellus sodales dignissim accumsan. Donec porta quis ipsum non feugiat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla at ligula porttitor, venenatis elit ut, lacinia mi. Praesent vel justo a urna convallis aliquam eu id nibh. Praesent sit amet porta diam. Aenean eget aliquet leo, vel molestie sapien. Donec tempus maximus orci. Nam malesuada, ex id eleifend fermentum, nisl odio fermentum quam, et sollicitudin enim ligula non ante. Duis feugiat et est eu malesuada. Nullam quis
-
-        </p>
-        <p class="article__item" contenteditable="true">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales neque eget consectetur condimentum. Phasellus sodales dignissim accumsan. Donec porta quis ipsum non feugiat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla at ligula porttitor, venenatis elit ut, lacinia mi. Praesent vel justo a urna convallis aliquam eu id nibh. Praesent sit amet porta diam. Aenean eget aliquet leo, vel molestie sapien. Donec tempus maximus orci. Nam malesuada, ex id eleifend fermentum, nisl odio fermentum quam, et sollicitudin enim ligula non ante. Duis feugiat et est eu malesuada. Nullam quis
+            <?= $data['text']; ?>
         </p>
     </div>
 </div>
